19aa86b663ff31f615fea09f61801dfaf87134ae
Bharat raj Singh <45910597+bharatrajsingh@users.noreply.github.com>
2019-10-18 19:16:09 +0530

    using print statement
    using print statement

--- iSMRITIChatbot.py
+++ iSMRITIChatbot.py
@@ -105,7 +105,7 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.20, rand
 
 
 def multi_hot_encode(sequences, dimension):
- 
+ print("hello")
   result=[]
   for i in sequences:
     g=[0]*dimension
